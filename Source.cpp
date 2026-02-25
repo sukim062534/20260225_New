d5713964c87111a124a2cb4f00ecae06954e289e
sukim062534 <sukim062534@gmail.com>
2026-02-25 10:17:41 +0900

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,14 +6,16 @@ int main()
 	int number1 = 0;
 	int number2 = 0;
 
-	cout << "숫자1 입력" << endl;
-	cin >> number1;
-	cout << "숫자1 입력" << endl;
-	cin >> number2;
-	cout << number1 + number2 << endl;
-	cout << number1 - number2 << endl;
-	cout << number1 * number2 << endl;
-	cout << number1 / number2 << endl;
-	cout << number1 % number2 << endl;
+	std:: cout << "숫자1 입력" << endl;
+	std:: cin >> number1;
+	std:: cout << "숫자2 입력" << endl;
+	std:: cin >> number2;
+
+	// \n 한줄띄기  \t Tap만큼 옆으로 '\b' 한칸 옆으로 \\역슬레쉬 하나 나옴
+	std::cout << number1 + number2 << '\n';
+	std:: cout << number1 - number2 << '\t';
+	std:: cout << number1 * number2 << '\b';
+	std:: cout << number1 / number2 << '\\';
+	std:: cout << number1 % number2 << endl;
 	return 0;
 }
