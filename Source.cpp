da565dc3e34e74c6ae840e7b22746b9f0f06d623
sukim062534 <sukim062534@gmail.com>
2026-02-25 15:41:53 +0900

    Update Source.cpp
    논리연산자

--- Source.cpp
+++ Source.cpp
@@ -5,75 +5,32 @@ using namespace std;
 
 int main()
 {
-	//2차원 맵
-	int Map[10][10]{
+	bool Result1 = true;
+	bool REsult2 = false;
+	bool REsult3 = false;
 
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	//연산자 우선 순위 ,산술, 논리, 비교
+	//() 안에 들어있는게 가장 먼저
+	// cout << ((Result1 || REsult2) && REsult3)<< endl; 를 풀어쓴게 위에것
 
-	};
+	bool Temp(Result1 || REsult2);
+	Temp = Temp && REsult3 && (10 > 3);
 
-	int PlayerX = 1;
-	int PlayerY = 1;
-
-	for (;;) //Frame, GameLoop
+	cout << Temp << endl;
+//
+	if (Result1 && REsult2) //두 조건 모두 참
 	{
-		for (int Y = 0; Y < 10; ++Y) //Render
-		{
-			for (int X = 0; X < 10; ++X)
-			{
-				if (PlayerX == X && PlayerY == Y) // && - 앞에 조건과 뒤에 조건이 모두 참일때 사용
-				{
-					cout << "%";
-				}
-				else if (Map[Y][X] == 1)
-				{
-					cout << "#";
-				}
-				else if (Map[Y][X] == 0)
-				{
-					cout << " ";
-				}
-
-			
-			}
-
-			cout << endl;
-		}
-
-		int Input;//input
-		Input = _getch(); //'w', 'a', 's','d'
-		//Process
-		if (Input == 'w')
-		{
-			PlayerY--;
-			
-		}
-		
-		if (Input == 's')
-		{
-			PlayerY++;
-		}
-		if (Input == 'a')
-		{
-			PlayerX--;
-		}
-		if (Input == 'd')
-		{
-			PlayerX++;
-		}
-		
-
-		system("cls");//실행된 화면 사라짐	
-
+		cout << "안녕" << endl;
 	}
+
+	if (Result1 || REsult2)//둘중 하나만 참 
+	{
+		cout << "안녕2" << endl;
+	}
+	if (!REsult2) //! = not
+	{
+		cout << "안녕3" << endl;
+	}
+
 	return 0;
 }
