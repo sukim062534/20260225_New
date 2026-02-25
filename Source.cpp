32e3ec91a318044e756084abc204a1f89071e2ef
sukim062534 <sukim062534@gmail.com>
2026-02-25 09:09:13 +0900

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number1 {};
+	int number2 {};
+	cout << "숫자 2개 입력" << endl;
+	cin >> number1;
+	cin >> number2;
+	cout << number1 + number2 << endl;
+	cout << number1 - number2 << endl;
+	cout << number1 * number2 << endl;
+	cout << number1 / number2 << endl;
+	cout << number1 % number2 << endl;
+	return 0;
+}
