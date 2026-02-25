c56af01a2a09e07fda9fa17320bbde1d0e45401c
sukim062534 <sukim062534@gmail.com>
2026-02-25 16:06:43 +0900

    아스키코드
    메모리에 저장되는 것은 다  숫자이다

--- Source.cpp
+++ Source.cpp
@@ -1,36 +1,20 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
-int main()
+int main() // 메모리에 저장된건 다 숫자이다.
 {
-	bool Result1 = true;
-	bool REsult2 = false;
-	bool REsult3 = false;
+	char Temp = 65;// 아스키코드 65를 넣어도 A 출력 
+	cout << Temp;
 
-	//연산자 우선 순위 ,산술, 논리, 비교
-	//() 안에 들어있는게 가장 먼저
-	// cout << ((Result1 || REsult2) && REsult3)<< endl; 를 풀어쓴게 위에것
+	char Temp = 'A'; //  A 출력 
+	cout << Temp;
 
-	bool Temp(Result1 || REsult2);
-	Temp = Temp && REsult3 && (10 > 3);
+	char Temp = 65 + 1; //B 출력
+	cout << Temp;
 
-	cout << Temp << endl;
-//
-	if (Result1 && REsult2) //두 조건 모두 참
-	{
-		cout << "안녕" << endl;
-	}
-
-	if (Result1 || REsult2)//둘중 하나만 참 
-	{
-		cout << "안녕2" << endl;
-	}
-	if (!REsult2) //! = not
-	{
-		cout << "안녕3" << endl;
-	}
+	char Temp = 'A' + 1; //B 출력
+	cout << Temp;
 
 	return 0;
 }
