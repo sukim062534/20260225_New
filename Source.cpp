e9e3d63ffe6d94751b570ef6f6028b9fa3d1a8c7
sukim062534 <sukim062534@gmail.com>
2026-02-25 09:40:43 +0900

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
-	int number1;
-	int number2;
-	cout << "숫자 2개" << endl;
+	int number1 = 0;
+	int number2 = 0;
+
+	cout << "숫자1 입력" << endl;
 	cin >> number1;
+	cout << "숫자1 입력" << endl;
 	cin >> number2;
 	cout << number1 + number2 << endl;
 	cout << number1 - number2 << endl;
