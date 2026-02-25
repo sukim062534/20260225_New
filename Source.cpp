0af6df91f299d0188820c7af3a246301c82c0e15
sukim062534 <sukim062534@gmail.com>
2026-02-25 15:22:19 +0900

    Update Source.cpp
    wasd로 만들기

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,79 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
 
 int main()
 {
-	int number1 = 0;
-	int number2 = 0;
+	//2차원 맵
+	int Map[10][10]{
 
-	std:: cout << "숫자1 입력" << endl;
-	std:: cin >> number1;
-	std:: cout << "숫자2 입력" << endl;
-	std:: cin >> number2;
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 
-	// \n 한줄띄기  \t Tap만큼 옆으로 '\b' 한칸 옆으로 \\역슬레쉬 하나 나옴
-	std::cout << number1 + number2 << '\n';
-	std:: cout << number1 - number2 << '\t';
-	std:: cout << number1 * number2 << '\b';
-	std:: cout << number1 / number2 << '\\';
-	std:: cout << number1 % number2 << endl;
+	};
+
+	int PlayerX = 1;
+	int PlayerY = 1;
+
+	for (;;) //Frame, GameLoop
+	{
+		for (int Y = 0; Y < 10; ++Y) //Render
+		{
+			for (int X = 0; X < 10; ++X)
+			{
+				if (PlayerX == X && PlayerY == Y) // && - 앞에 조건과 뒤에 조건이 모두 참일때 사용
+				{
+					cout << "%";
+				}
+				else if (Map[Y][X] == 1)
+				{
+					cout << "#";
+				}
+				else if (Map[Y][X] == 0)
+				{
+					cout << " ";
+				}
+
+			
+			}
+
+			cout << endl;
+		}
+
+		int Input;//input
+		Input = _getch(); //'w', 'a', 's','d'
+		//Process
+		if (Input == 'w')
+		{
+			PlayerY--;
+			
+		}
+		
+		if (Input == 's')
+		{
+			PlayerY++;
+		}
+		if (Input == 'a')
+		{
+			PlayerX--;
+		}
+		if (Input == 'd')
+		{
+			PlayerX++;
+		}
+		
+
+		system("cls");//실행된 화면 사라짐	
+
+	}
 	return 0;
 }
